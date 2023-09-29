8391328d16503e26f471146e3ea5ee01081a405d
vkoesmahargyo <vidya.koesmahargyo@gmail.com>
2023-09-29 13:01:53 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -43,6 +43,7 @@
           var height = byCodes('8302-2');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
 
@@ -52,6 +53,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
+          p.madeuplabel = getQuantityValueAndUnit(height[1]);
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
